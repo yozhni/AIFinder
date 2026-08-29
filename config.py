3de02a9ffe46daeb010417167b41fc9478f4bc31
Anna Grigoryeva <yozhni@gmail.com>
2026-08-29 08:46:24 +0800

    Move API key to env var, add .env support

--- config.py
+++ config.py
@@ -1,9 +1,26 @@
 """Load configuration from config.yaml."""
 
 import os
+import re
 import yaml
 
 _config = None
+
+
+def _resolve_env_vars(value):
+    """Resolve ${ENV_VAR} patterns in config values."""
+    if isinstance(value, str):
+        pattern = r'\$\{(\w+)\}'
+        matches = re.findall(pattern, value)
+        for var_name in matches:
+            env_value = os.environ.get(var_name, '')
+            value = value.replace(f'${{{var_name}}}', env_value)
+        return value
+    elif isinstance(value, dict):
+        return {k: _resolve_env_vars(v) for k, v in value.items()}
+    elif isinstance(value, list):
+        return [_resolve_env_vars(item) for item in value]
+    return value
 
 
 def load_config():
@@ -13,6 +30,7 @@ def load_config():
         config_path = os.path.join(os.path.dirname(__file__), "config.yaml")
         with open(config_path, "r") as f:
             _config = yaml.safe_load(f)
+        _config = _resolve_env_vars(_config)
     return _config
 
 
